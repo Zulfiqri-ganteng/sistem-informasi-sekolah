95c13f920a6d72cec927c1044b19dd336bd954e3
zulfiqri-ganteng <zf.zulfiqri@gmail.com>
2025-11-16 03:49:43 +0700

    force deploy clean

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -229,7 +229,7 @@
                 Belum punya akun? <a href="<?= smart_url('register-siswa') ?>">Daftar Disini....!!</a>
             </div>
 
-            <footer>© <?= date('Y') ?> Sistem Informai Akademik </footer>
+            <footer>© <?= date('Y') ?> Sistem Informai </footer>
         </div>
     </div>
 
