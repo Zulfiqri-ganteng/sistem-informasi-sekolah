2997bb49dcc6b845b0d0734d1f0ec4d2b9c943c6
zulfiqri-ganteng <zf.zulfiqri@gmail.com>
2025-11-17 11:58:00 +0700

    menambahkan configurasi hosting

--- public/index.php
+++ public/index.php
@@ -4,56 +4,53 @@ use CodeIgniter\Boot;
 use Config\Paths;
 
 /*
- *---------------------------------------------------------------
  * CHECK PHP VERSION
- *---------------------------------------------------------------
  */
 
-$minPhpVersion = '8.1'; // If you update this, don't forget to update `spark`.
+$minPhpVersion = '8.1';
 if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
-    $message = sprintf(
-        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
-        $minPhpVersion,
-        PHP_VERSION,
-    );
-
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo $message;
-
-    exit(1);
+    die("PHP version must be {$minPhpVersion} or higher");
 }
 
 /*
- *---------------------------------------------------------------
- * SET THE CURRENT DIRECTORY
- *---------------------------------------------------------------
+ * FRONT CONTROLLER
  */
-
-// Path to the front controller (this file)
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
-// Ensure the current directory is pointing to the front controller's directory
-if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
-    chdir(FCPATH);
-}
+/*
+ * DETEKSI MODE (LOKAL / HOSTING)
+ */
+$isLocal = (php_sapi_name() === 'cli-server' || strpos($_SERVER['HTTP_HOST'] ?? '', 'localhost') !== false);
 
 /*
- *---------------------------------------------------------------
- * BOOTSTRAP THE APPLICATION
- *---------------------------------------------------------------
- * This process sets up the path constants, loads and registers
- * our autoloader, along with Composer's, loads our constants
- * and fires up an environment-specific bootstrapping.
+ * SET ROOT PROJECT
+ *
+ * Lokal:
+ *   project ada di ../ (default CI4)
+ *
+ * Hosting:
+ *   mas menaruh project langsung di:
+ *   /home/zulh7811/public_html/sekolah-galajuara.zulfiqri.com/
  */
+if ($isLocal) {
+    // LOKAL
+    $rootPath = __DIR__ . '/../';
+} else {
+    // HOSTING
+    $rootPath = __DIR__ . '/';
+}
 
-// LOAD OUR PATHS CONFIG FILE
-// This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../app/Config/Paths.php';
-// ^^^ Change this line if you move your application folder
+chdir($rootPath);
 
+/*
+ * LOAD PATHS CONFIG
+ */
+require $rootPath . 'app/Config/Paths.php';
 $paths = new Paths();
 
-// LOAD THE FRAMEWORK BOOTSTRAP FILE
+/*
+ * BOOTSTRAP
+ */
 require $paths->systemDirectory . '/Boot.php';
 
 exit(Boot::bootWeb($paths));
