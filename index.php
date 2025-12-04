4102fc6f13c3a0ef1af2ced9bce0487f871585a5
zulfiqri-ganteng <zf.zulfiqri@gmail.com>
2025-12-05 01:09:39 +0700

    dfdf

--- index.php
+++ index.php
@@ -1,210 +1,38 @@
 <?php
-
-/*
- *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
- *---------------------------------------------------------------
- *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
- *
- * This can be set to anything, but default usage is:
- *
- *     development
- *     testing
- *     production
- *
- * NOTE: If you change these, also change the error_reporting() code below
- */
-define('ENVIRONMENT', isset($_SERVER['CI_ENVIRONMENT']) ? $_SERVER['CI_ENVIRONMENT'] : 'production');
-
-/*
- *---------------------------------------------------------------
- * ERROR REPORTING
- *---------------------------------------------------------------
- *
- * Different environments will require different levels of error reporting.
- * By default development will show errors but testing and live will hide them.
- */
-switch (ENVIRONMENT) {
-    case 'development':
-        error_reporting(-1);
-        ini_set('display_errors', 1);
-        break;
-    case 'testing':
-    case 'production':
-        ini_set('display_errors', 0);
-        error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT);
-        break;
-    default:
-        header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-        echo 'The application environment is not set correctly.';
-        exit(1); // EXIT_ERROR
-}
-
-/*
- *---------------------------------------------------------------
- * SYSTEM DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * This variable must contain the name of your "system" directory.
- * Set the path if it is not in the same directory as this file.
- */
-$systemPath = 'system';
-
-/*
- *---------------------------------------------------------------
- * APPLICATION DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * If you want this front controller to use a different "application"
- * directory than the default one you can set its name here. The directory
- * can also be renamed or relocated anywhere on your server. If you do,
- * use an absolute (full) server path.
- * For more info please see the user guide:
- *
- * https://codeigniter.com/user_guide/general/managing_apps.html
- *
- * NO TRAILING SLASH!
- */
-$applicationFolder = 'app';
-
-/*
- *---------------------------------------------------------------
- * VIEW DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * If you want to move the view directory out of the application
- * directory, set the path to it here. The directory can be renamed
- * and relocated anywhere on your server. If blank, it will default
- * to the standard location inside your application directory.
- * If you do move this, use an absolute (full) server path.
- *
- * NO TRAILING SLASH!
- */
-$viewFolder = '';
-
-/*
- * --------------------------------------------------------------------
- * DEFAULT CONTROLLER
- * --------------------------------------------------------------------
- *
- * Normally you will set your default controller in the routes.php file.
- * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here. For most applications, you
- * WILL NOT set your routing here, but it's an option for those
- * special instances where you might want to override the standard
- * routing in a specific front controller that shares a common CI installation.
- *
- * IMPORTANT: If you set the routing here, NO OTHER controller will be
- * callable. In essence, this preference limits your application to ONE
- * specific controller. Leave the function name blank if you need
- * to call functions dynamically via the URI.
- *
- * Un-comment the $routing array below to use this feature
- */
-// $routing['controller'] = '';
-
-/*
- * --------------------------------------------------------------------
- *  CUSTOM CONFIG VALUES
- * --------------------------------------------------------------------
- *
- * The $assign_to_config array below will be passed to the Config class
- * when initialized. This allows you to set custom config items or
- * override any default config values found in the config.php file.
- * This can be handy as it permits you to share one application between
- * multiple front controller files, with each file containing different
- * config values.
- *
- * Un-comment the $assign_to_config array below to use this feature
- */
-// $assign_to_config['name_of_config_item'] = 'value of config item';
-
-// --------------------------------------------------------------------
-// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
-// --------------------------------------------------------------------
-
-/*
- * ---------------------------------------------------------------
- *  Resolve the system path for increased reliability
- * ---------------------------------------------------------------
- */
-
-// Set the current directory correctly for CLI requests
-if (defined('STDIN')) {
-    chdir(dirname(__FILE__));
-}
-
-if (($_temp = realpath($systemPath)) !== FALSE) {
-    $systemPath = $_temp . DIRECTORY_SEPARATOR;
-} else {
-    // Ensure there's a trailing slash
-    $systemPath = strtr(
-            rtrim($systemPath, '/\\'),
-            '/\\',
-            DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-        ) . DIRECTORY_SEPARATOR;
-}
-
-// Is the system path correct?
-if (!is_dir($systemPath)) {
-    header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-    echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: ' . pathinfo(__FILE__, PATHINFO_BASENAME);
-    exit(3); // EXIT_CONFIG
-}
-
-/*
- * -------------------------------------------------------------------
- *  Now that we know the path, set the main path constants
- * -------------------------------------------------------------------
- */
-// The name of THIS file
-define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
-
-// Path to the system directory
-define('BASEPATH', $systemPath);
-
-// Path to the front controller (this file) directory
+// Path to the front controller (this file)
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
-// Name of the "system" directory
-define('SYSDIR', basename(BASEPATH));
-
-// The path to the "application" directory
-if (is_dir($applicationFolder)) {
-    if (($_temp = realpath($applicationFolder)) !== FALSE) {
-        $applicationFolder = $_temp;
-    }
-    define('APPPATH', $applicationFolder . DIRECTORY_SEPARATOR);
-} else {
-    if (!is_dir(BASEPATH . $applicationFolder . DIRECTORY_SEPARATOR)) {
-        header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-        echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: ' . SELF;
-        exit(3); // EXIT_CONFIG
-    }
-    define('APPPATH', BASEPATH . $applicationFolder . DIRECTORY_SEPARATOR);
-}
-
-// The path to the "views" directory
-if (!isset($viewFolder[0]) && is_dir(APPPATH . 'views' . DIRECTORY_SEPARATOR)) {
-    $viewFolder = APPPATH . 'views';
-}
-if (is_dir($viewFolder)) {
-    if (($_temp = realpath($viewFolder)) !== FALSE) {
-        $viewFolder = $_temp;
-    }
-    define('VIEWPATH', $viewFolder . DIRECTORY_SEPARATOR);
-} else {
-    define('VIEWPATH', APPPATH . 'views' . DIRECTORY_SEPARATOR);
-}
+// Ensure the current directory is pointing to the front controller's directory
+chdir(FCPATH);
 
 /*
- * --------------------------------------------------------------------
- * LOAD THE BOOTSTRAP FILE
- * --------------------------------------------------------------------
- *
- * And away we go...
+ *---------------------------------------------------------------
+ * BOOTSTRAP THE APPLICATION
+ *---------------------------------------------------------------
+ * This process sets up the path constants, loads and registers
+ * our autoloader, along with Composer's, loads our constants
+ * and fires up an environment-specific bootstrapping.
  */
-require_once BASEPATH . 'system.php';
+
+// Path to the project root directory. Just above FCPATH
+$pathsPath = realpath(FCPATH . '../app/Config/Paths.php') ?: (realpath(FCPATH . 'app/Config/Paths.php') ?: false);
+
+if (!$pathsPath) {
+    header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+    echo 'Your application folder path does not appear to be set correctly.';
+    exit(1);
+}
+
+define('APPPATH', $pathsPath);
+require $pathsPath;
+
+/*
+ *---------------------------------------------------------------
+ * LAUNCH THE APPLICATION
+ *---------------------------------------------------------------
+ * Now that everything is set up, it's time to actually fire
+ * up the application and make it run!
+ */
+
+$app = require APPPATH . '../bootstrap.php';
+$app->run();
